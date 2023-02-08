714cdc6fa7767eb4933a2be811de4f26fc68874a
Ricky Wu <rickywu209@gmail.com>
2023-02-08 15:14:16 -0700

    adding new task button and logic

--- src/component/AddTask.jsx
+++ src/component/AddTask.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 const AddTask = () => {
-    return (
+    const [addModal, setAddModal] = useState(false);
+
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white uppercase text-sm 
+            font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70"
+            type="button"
+            //when button is clicked we set the modal to true
+            onClick={() => setAddModal(true)}
+      >
+        +New
+      </button>
+      {addModal ? (
         <>
-            <button className="bg-blue-500 text-white uppercase text-sm font-semibold 
-            py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70">+New</button>
+            <div className="flex items-center justify-center overflow-x-hidden
+            overflow-y-auto fixed inset-0 z-100">
+                <h3>Add New Task</h3>
+            </div>
         </>
-    )
-}
+      ):null}
+    </>
+  );
+};
 
 export default AddTask;
