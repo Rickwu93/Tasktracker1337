9d93158cf30597e08947bd2dab0721bc9cc59cf6
Ricky Wu <rickywu209@gmail.com>
2023-02-12 00:42:53 -0700

    adding task closes the viewport

--- src/component/AddTask.jsx
+++ src/component/AddTask.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-
+//opening views for adding new task
 const AddTask = () => {
   const [addModal, setAddModal] = useState(false);
+
+  //when add task button is clicked we need to close the box
+  const handleAdd = () => {
+    setAddModal(false)
+  }
 
   return (
     <>
@@ -19,8 +24,8 @@ const AddTask = () => {
           <div
             className="flex items-center justify-center overflow-x-hidden
             overflow-y-auto fixed inset-0 z-100">
-            <div className="w-9/12 bg-white rounded-lg shadow-md relative flex flex-col">
-            <div className="flex flex-row justify-between p-5">
+            <div className="w-9/12 max-w-lg bg-white rounded-lg shadow-md relative flex flex-col">
+            <div className="flex flex-row justify-between p-5 border-b border-slate-200 rounded-t">
               <h3 className="text-3xl font-semibold">Add New Task</h3>
               <button
                 className="px-1 text-gray-400 float-right text-3xl
@@ -57,7 +62,7 @@ const AddTask = () => {
                     className="w-full bg-gray-200 text-gray-700 border border-gray-200 
                     rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
                         id="task-description"
-                        rows="3"
+                        rows="5"
                         placeholder="Task description"
                     />
                 </div>
@@ -65,9 +70,11 @@ const AddTask = () => {
             <div>
               <button></button>
             </div>
-            <div>
+            <div className="flex justify-end p-6 border-t border-slate-200 rounded-b">
                 <button className="bg-blue-500 text-white font-semibold uppercase text-sm
-                px-6 py-3 rounded hover:opacity-70">
+                px-6 py-3 rounded hover:opacity-70"
+                onClick={handleAdd}
+                >
                     Add Task
                 </button>
             </div>
