d1370eef6cc08c5a1d37cb686a77ca08f25e7f0a
Ricky Wu <rickywu209@gmail.com>
2023-02-17 00:00:00 -0700

    adding basic edit and delete buttons to form

--- src/App.js
+++ src/App.js
@@ -9,20 +9,22 @@ function App() {
 
   return (
     <>
-      <h1 className="text-2xl font-bold py-4 pl-6">Task Tracker</h1>
+      <h1 className="text-2xl font-bold py-6 pl-6">Task Tracker</h1>
       <p className="text-xl pl-6">Hello testing</p>
       <div className="flex flex-row items-center">
       <p className="text-xl pl-6">Click</p> 
         <AddTask taskList={taskList} setTaskList={setTaskList} />
       <p className="text-xl my-2">to add a new task</p>
       </div>
+      <div>
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg 
+        my-4 py-1 px-2 bg-gray-200">To Do:</h2>
       {taskList.map((task, i) =>
         <>
-        <ToDo />
-          <p>{task.projectName}</p>
-          <p>{task.taskDescription}</p>
+        <ToDo key={i} task={task} />
         </>
       )}
+      </div>
     </>
   );
 } 
