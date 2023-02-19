520c5649e61791a73f4057aff75be2aa5a043736
Ricky Wu <rickywu209@gmail.com>
2023-02-18 23:37:35 -0700

    adding working delete button to remove last index

--- src/component/ToDo.jsx
+++ src/component/ToDo.jsx
@@ -1,24 +1,42 @@
 import EditTask from "./EditTask";
 
 const ToDo = ({ task, index, taskList, setTaskList }) => {
+  //getting index number we want to delete
+    const handleDelete = itemID => {
+        let removeIndex = taskList.indexOf(task);
+        taskList.splice(removeIndex, 1);
+        //filter through taskList to show what's left after removal
+        setTaskList((currentTasks => currentTasks.filter
+            (todo => todo.id !== itemID)))
+    }
     return (
-        <>
-            <div className="flex flex-col items-start justify-start bg-white
-            my-4 ml-6 py-4 px-6 w-3/4 max-w-lg">
-                <div className="w-full flex flex-row justify-between">
-                    <p className="font-semibold text-xl">
-                    {task.projectName}</p>
-                    <EditTask task={task} index={index} taskList={taskList} 
-                    setTaskList={setTaskList} />
-                </div>
-                 <p className="text-lg py-2">{task.taskDescription}</p>
-                 <div className="w-full flex justify-center">
-                    <button className="bg-red-500 text-white text-sm uppercase
-                    font-semibold py-1.5 px-3 mt-6 mb-1 rounded-lg">Delete</button>
-                 </div>
-            </div>
-        </>
-    )
-}
+    <>
+      <div
+        className="flex flex-col items-start justify-start bg-white
+            my-4 ml-6 py-4 px-6 w-3/4 max-w-lg"
+      >
+        <div className="w-full flex flex-row justify-between">
+          <p className="font-semibold text-xl">{task.projectName}</p>
+          <EditTask
+            task={task}
+            index={index}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        </div>
+        <p className="text-lg py-2">{task.taskDescription}</p>
+        <div className="w-full flex justify-center">
+          <button
+            className="bg-red-500 text-white text-sm uppercase
+                        font-semibold py-1.5 px-3 mt-6 mb-1 rounded-lg"
+          onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ToDo;
